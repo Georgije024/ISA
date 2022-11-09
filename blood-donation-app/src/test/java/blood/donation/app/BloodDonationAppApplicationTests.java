a43e749d95abf04bca08abece94a2574d62a15d8
unknown <tanasic.ra229.2018@uns.ac.rs>
2022-11-09 17:51:59 +0100

    Napravljeni projekti

---
+++ blood-donation-app/src/test/java/blood/donation/app/BloodDonationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package blood.donation.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
